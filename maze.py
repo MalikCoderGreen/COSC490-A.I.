92cf2b17e4126bbc34be0920cfd208166a05c375
Malik Green <green.malik5@gmail.com>
2020-09-17 13:46:45 -0400

    Update maze.py
    Now includes portal (red)
    Need character and random Walk

--- maze.py
+++ maze.py
@@ -1,53 +1,77 @@
 import pygame, sys
 import random
+import numpy as np
+#import df.py as df
 
+
+#Colors
+Black = (0, 0, 0)
+White = (255, 255, 255)
+Green = (0, 255, 0)
+Red = (255, 0, 0)
 
 pygame.init()
 
 #variables for screen
-screen_w = 1000
-screen_h = 1000
+screen_w = 255
+screen_h = 255
 screen = pygame.display.set_mode((screen_w, screen_h))
 
 
 font = pygame.font.Font(None, 100)
 
-txt = font.render("#", 1500,  (65, 34, 140))
-txt_pos = txt.get_rect()
-bg = pygame.Surface(screen.get_size())
-bg = bg.convert()
 
-# position symbol in top left of screen.
-txt_pos.topleft = bg.get_rect().topleft
-bg.fill((80, 80, 80))
-
+screen.fill(Black)
 
 #tmp_sq = pygame.draw.rect(screen, [50, 40, 100], [0, 0, 400, 400], 0)
+# for grid screen.
+W = 20
+H = 20
+Marg = 5
+grid = []
+
+""" This will be the matrix 'grid' that will represent the map. """
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+
+
+for row in range(10):
+    for col in range(10):
+
+        color = White
 
 
 
-
-# vertixal axis variable.
-x = 0
-#generate maze.
-for i in range (0, 1500):
-    r = random.randint(0, 1000)
-    tmp_txt = txt
-    tmp_txt = tmp_txt.get_rect()
-    bg.blit(txt, tmp_txt.move(x, r))
-    screen.blit(bg, (0, 0))
-
-    x += 50
+        pygame.draw.rect(screen, color, [(Marg + W) * col + Marg,
+                              (Marg + H) * row + Marg,
+                              W,
+                              H])
 
 
+x = random.randrange(0, 10)
+y = random.randrange(0, 10)
 
+
+pygame.draw.rect(screen, Red, [(Marg + W) * y + Marg,
+                      (Marg + H) * x + Marg,
+                      W,
+                      H])
+print(x)
 #screen set up.
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    screen.blit(bg, (0, 0))
+        #elif event.type == pygame.MOUSEBUTTONDOWN:
+
+
+
+
 
     pygame.display.flip()
 
